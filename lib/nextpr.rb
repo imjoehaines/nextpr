e0018ed51a1cded1a0eb062bd7a083af41950901
Joe Haines <hello@joehaines.co.uk>
2021-02-07 14:15:14 +0000

    Improve error handling

--- lib/nextpr.rb
+++ lib/nextpr.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-require "json"
-require "net/http"
-
 require_relative "nextpr/github"
 
 module NextPr
@@ -10,26 +7,10 @@ module NextPr
 
   class << self
     def find(token, owner, repository)
-      github = GitHub.new
-      query = github.query(owner, repository)
+      github = GitHub.new(token)
+      ids = github.get_ids(owner, repository)
 
-      response = Net::HTTP.post(
-        GitHub::ENDPOINT,
-        JSON.generate({ query: query }),
-        {
-          "Content-Type" => "application/json",
-          "Authorization" => "bearer #{token}"
-        }
-      )
-
-      body = JSON::parse(response.body)
-
-      raise body["errors"].to_s if body["errors"]
-
-      issueNumber = body["data"]["repository"]["issues"]["edges"][0]["node"]["number"]
-      prNumber = body["data"]["repository"]["pullRequests"]["edges"][0]["node"]["number"]
-
-      [issueNumber, prNumber].max + 1
+      ids.max.next
     end
   end
 end
--- lib/nextpr/github.rb
+++ lib/nextpr/github.rb
@@ -1,10 +1,34 @@
 # frozen_string_literal: true
 
+require "json"
+require "net/http"
+
 require_relative "../nextpr"
 
 module NextPr
   class GitHub
-    ENDPOINT = URI("https://api.github.com/graphql")
+    def initialize(token)
+      @headers = {
+        "Content-Type" => "application/json",
+        "Authorization" => "bearer #{token}"
+      }.freeze
+    end
+
+    def get_ids(owner, repository)
+      response = Net::HTTP.post(
+        URI("https://api.github.com/graphql"),
+        JSON.generate({ query: query(owner, repository) }),
+        @headers
+      )
+
+      body = JSON::parse(response.body)
+
+      raise body["errors"].map { |error| error["message"] }.join("\n") if body["errors"]
+
+      extract_ids(body)
+    end
+
+    private
 
     def query(owner, repository)
       <<~GRAPHQL
@@ -27,6 +51,16 @@ module NextPr
         }
       }
       GRAPHQL
+    end
+
+    def extract_ids(body)
+      ["issues", "pullRequests"].map do |key|
+        edges = body["data"]["repository"][key]["edges"]
+
+        next 0 if edges.empty?
+
+        edges[0]["node"]["number"]
+      end
     end
   end
 end
